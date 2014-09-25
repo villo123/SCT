bfa64c8ef0debdf4d63addb72abf5975edc76d2a
7h3n3w3r4cr4ck3r <andres0149502@gmail.com>
2014-09-25 15:25:41 -0500

    Vista exporte a excel

---
+++ WebContent/js/vista.js
@@ -0,0 +1,24 @@
+	function desaparecer_select(){
+		document.getElementById("select").style.visibility="hidden";
+	}
+	function desaparecer_botones(){
+		document.getElementById("btn").style.visibility="hidden";
+	}
+	function aparecer_select(){
+		document.getElementById("select").style.visibility="hidden";
+	}
+	function aparecer_botones(){
+		document.getElementById("btn").style.visibility="hidden";
+	}
+	/*
+	 * Introduzca la siguiente linea en el archivo que desea (vista, vistas, vistausuario)
+	 * <head>
+	 * <script type="text/javascript" src="js/vista.js"></script>
+	 * </head>
+	 * 
+	 * Para hacer surtir efectos, solo necesita crear un boton de la siguiente manera.
+	 * <button id="btn" onclick="desaparecer_select();desaparecer_botones();">Antes de exportar presione aquí</button>
+	 * <button id="btn" onclick="aparecer_select();aparecer_botones();">Despues de exportar presione aquí</button>
+	 * Una vez creado, antes de exportar a excel, presione el primer boton
+	 * una ves exportado presione el boton dos.. 
+	 */
