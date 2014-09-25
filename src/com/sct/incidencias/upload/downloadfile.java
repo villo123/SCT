61d2698474088c26e38ac698faa820f51f8860e9
7h3n3w3r4cr4ck3r <andres0149502@gmail.com>
2014-09-25 15:27:59 -0500

    upload terminado

--- src/com/sct/incidencias/upload/downloadfile.java
+++ src/com/sct/incidencias/upload/downloadfile.java
@@ -1,5 +1,14 @@
 package com.sct.incidencias.upload;
 
+import java.io.IOException;
+import java.io.OutputStream;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 public class downloadfile extends HttpServlet{
 	   
 	  public void init(ServletConfig config) throws ServletException{
