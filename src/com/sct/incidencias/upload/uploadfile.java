d2bf187d173bb000604a61070cad1483f27164e1
7h3n3w3r4cr4ck3r <andres0149502@gmail.com>
2014-09-25 15:35:31 -0500

    Upload casi terminado

--- src/com/sct/incidencias/upload/uploadfile.java
+++ src/com/sct/incidencias/upload/uploadfile.java
@@ -1,11 +1,35 @@
 package com.sct.incidencias.upload;
 
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.Iterator;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Cookie;
+
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
+import org.apache.tomcat.jni.File;
+
+import com.sun.xml.internal.bind.v2.schemagen.xmlschema.List;
+
+
 public class uploadfile extends HttpServlet {
 	public void init(ServletConfig config) throws ServletException{
 	}		
 	super.init(config);
 }
-protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws    
+
+protected void processRequest (HttpServletRequest request, HttpServletResponse response) throws ServletException,IOException{   
 	// Validación de documentos menores a 4MB
 	DiskFileItemFactory factory = new DiskFileItemFactory();
 	factory.setSizeThreshold(4 * 1024 * 1024);
@@ -36,16 +60,18 @@ protected void processRequest(HttpServletRequest request, HttpServletResponse re
 	                "PWD",
 	                "ITEM-TYPE",
 	                keys, values, bArchivo);
+	             }
 	               
-	               
-	            } catch(Exception e){
-	                lSuccess = false;
-	            }  finally{           
-	                response.getWriter().println("Subió documento: " + lSuccess);
-	            }           
-	       }
-	      
-	       protected void doPost(HttpServletRequest request,HttpServletResponse response) throws ServletException,IOException{
-	           processRequest(request,response);
-	       }
+	            }
+		}
+	catch(Exception e){
+		lSuccess = false;
+	}
+	finally{           
+		response.getWriter().println("Subió documento: " + lSuccess);
+	   }           
+}  
+	protected void doPost(HttpServletRequest request,HttpServletResponse response) throws ServletException,IOException{
+		processRequest(request,response);
+	}
 }
