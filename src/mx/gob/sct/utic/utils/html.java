8871397d2c21a21e41ce084ba673b9ee08c75651
villo123 <villo_m23@hotmail.com>
2014-10-15 10:22:05 -0500

    Se quito los "Nulls" de vista

--- src/mx/gob/sct/utic/utils/html.java
+++ src/mx/gob/sct/utic/utils/html.java
@@ -25,35 +25,7 @@ public class html {
 		ResultSet rs = null;
 		new INCIDENCIA();
 		new CATUnidadResponsable();
-
-		/*
-		 * DBConexion db = new DBConexion(); try { PreparedStatement ps =
-		 * db.getCt
-		 * ().prepareStatement("INSERT INTO PUB.INCIDENCIA (FAtencion) VALUES (?)"
-		 * ); ps.setDate(1,(java.sql.Date) inc.getFAtencion());
-		 * ps.executeUpdate();
-		 * System.out.println("Se efectuo correctamente el Insert");
-		 * db.getCt().close(); } catch (SQLException e) { e.printStackTrace();
-		 * System.out.println("No se hizo insert"); }
-		 */
-		/*
-		 * catunr.setildUnidadAdm(1); inc.setFolio("345yiyi");
-		 * inc.setAprobado("SI"); String FechaAprobacion = "2010-12-24";
-		 * java.sql.Date fecha=java.sql.Date.valueOf(FechaAprobacion);
-		 * inc.setFechaAprobacion(fecha);
-		 * inc.setDescripcionIncidencia("Descripcion Incidencia");
-		 * inc.setPrioridad("Alta"); String FELab = "2010-12-26"; java.sql.Date
-		 * fecha2 = java.sql.Date.valueOf(FELab); inc.setFELab(fecha2); String
-		 * horaLab = "13:35"; java.sql.Time hELab
-		 * =java.sql.Time.valueOf(horaLab+":00"); inc.setHELab(hELab); String
-		 * FAtencion = "2010-11-23"; java.sql.Date FAtencion2 =
-		 * java.sql.Date.valueOf(FAtencion); inc.setFAtencion(FAtencion2);
-		 * String HAtencion = "12:45" ; java.sql.Time HAtencion2
-		 * =java.sql.Time.valueOf(HAtencion+":00");
-		 * inc.setHAtencion(HAtencion2); inc.setRespuestaUtic("Respuesta utic");
-		 */
 		DBConexion d = new DBConexion();
-
 		try {
 			ct = d.getCt();
 			PreparedStatement ps2 = ct
@@ -121,14 +93,44 @@ public class html {
 				campos[12] = HELab;
 				
 				campos[13] = rs.getString(16)+ " " + rs.getString(17) + " " + rs.getString(18);
-				campos[14] = rs.getString(19)+ " " + rs.getString(20) + " " + rs.getString(21);
-				campos[15] = rs.getString(22);
 				
-				/*Date date2 = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.S").parse(rs.getString(23));
-				String HAtencion = new SimpleDateFormat("H:mm").format(date2); // 9:00*/
+				//campos[14] = rs.getString(19)+ " " + rs.getString(20) + " " + rs.getString(21);
+				if(rs.getString(19) == null && rs.getString(20) == null && rs.getString(21) == null){
+					campos[14] = "";
+				}else{
+					campos[14] = rs.getString(19)+ " " + rs.getString(20) + " " + rs.getString(21);
+				}
+				
+				campos[15] = rs.getString(22);
+				if(campos[15] == null){
+					campos[15] = "";
+				}else{
+					campos[15] = rs.getString(22);
+				}
+				
 				campos[16] = rs.getString(23);
+				if(campos[16] == null){
+					campos[16] = "";
+				}else{
+					Date date2 = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.S").parse(rs.getString(23));
+					String HAtencion = new SimpleDateFormat("H:mm").format(date2); // 9:00
+					campos[16] = HAtencion;
+				}
+				
 				campos[17] = rs.getString(24);
+				if(campos[17] == null){
+					campos[17] = "";
+				}else{
+					campos[17] = rs.getString(24);
+				}
+				
 				campos[18] = rs.getString(25);
+				if(campos[18] == null){
+					campos[18] = "";
+				}else{
+					campos[18] = rs.getString(25);
+				}
+				
 				campos[19] = rs.getString(26);
 				campos[20] = rs.getString(27);
 				
@@ -161,36 +163,9 @@ public class html {
 				
 				campos[24] = rs.getString(31);
 				campos[25] = rs.getString(32);
-				//campos[26] = rs.getString(33);
-				//campos[26] = rs.getString(3);
-				//campos[27] = rs.getString(32);
 				registros.add(campos);
-				
-				/*System.out.println(rs.getString(1) + " " + rs.getString(2)
-						+ " " + rs.getString(3) + " " + rs.getString(4) + " "
-						+ rs.getString(5) + " " + rs.getString(6) + " "
-						+ rs.getString(7) + " " + rs.getString(8) + " "
-						+ rs.getString(9) + " " + rs.getString(10) + " "
-						+ rs.getString(11) + " " + rs.getString(12) + " "
-						+ rs.getString(13) + " " + rs.getString(14) + " "
-						+ rs.getString(15) + " " + rs.getString(16) + " "
-						+ rs.getString(17) + " " + rs.getString(18) + " "
-						+ rs.getString(19) + " " + rs.getString(20) + " "
-						+ rs.getString(21) + " " + rs.getString(22) + " "
-						+ rs.getString(23) + " " + rs.getString(24) + " "
-						+ rs.getString(25) + " " + rs.getString(26) + " "
-						+ rs.getString(27) + " " + rs.getString(28) + " "
-						+ rs.getString(29) + " " + rs.getString(30) + " "
-						+ rs.getString(31) + " " + rs.getString(32) + " "
-						+ rs.getString(33) + " " + rs.getString(34) + " "
-						+ rs.getString(35) + " " + rs.getString(36) + " "
-						+ rs.getString(37) + " " + rs.getString(38) + " "
-						+ rs.getString(39) + " " + rs.getString(40) + " "
-						+ rs.getString(41) + " " + rs.getString(42));*/
 			}
 		} catch (SQLException e) {
-			// System.out.println("No se pudo insertar");
-			e.printStackTrace();
 			System.out.println();
 		} finally {
 			d.liberarConexion(ct);
